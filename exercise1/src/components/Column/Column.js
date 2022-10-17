993fe42408d0d0857a1b9ba9fdd354e89adf58f9
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2022-10-17 18:45:35 +0200

    Delete unnesecery files. Add Card list

--- exercise1/src/components/Column/Column.js
+++ exercise1/src/components/Column/Column.js
@@ -1,10 +1,18 @@
+import Card from '../Card/Card.js';
+import CardForm from '../CardForm/CardForm.js';
+
 import styles from './Column.module.scss';
 
-const Column = ({ title, icon }) => {
+const Column = ({ id, title, icon, cards, action }) => {
+
     return (
         <article className={styles.column}>
             <span className={`${styles.icon} fa fa-${icon}`}></span>
             <h3 className={styles.title}> {title}</h3>
+            <ul className={styles.cards}>
+                {cards.map(card => <Card key={card.id} title={card.title}></Card>)}
+            </ul>
+            <CardForm columnId={id} action={action} />
         </article>
     );
 };
