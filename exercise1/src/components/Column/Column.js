b0fa821a07511179a1341f75fe48ca7d98b70ac8
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2022-10-26 18:32:45 +0200

    Add search functionality

--- exercise1/src/components/Column/Column.js
+++ exercise1/src/components/Column/Column.js
@@ -7,7 +7,20 @@ import styles from './Column.module.scss';
 
 const Column = ({ id, title, icon, action }) => {
 
-    const cards = useSelector(state => state.cards.filter(card => card.columnId === id));
+
+    const cards = useSelector(state => state.cards
+        .filter(card => card.columnId === id)
+        .filter(card => {
+            if (state.searchString === ``) {
+                return true
+            } else {
+                if (card.title.toLowerCase().includes(state.searchString.searchString.toLowerCase())) {
+                    return true
+                } else {
+                    return false
+                }
+            }
+        }));
 
     return (
         <article className={styles.column}>
--- exercise1/src/components/SearchForm/SearchForm.js
+++ exercise1/src/components/SearchForm/SearchForm.js
@@ -1,12 +1,24 @@
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
 
 import styles from './SearchForm.module.scss';
 
 const SearchForm = () => {
+    const [searchText, setSearchText] = useState(``);
+
+    const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch({ type: 'UPDATE_SEARCHSTRING', payload: { searchString: searchText } });
+    }
+
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search" />
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
+            <TextInput placeholder="Search" value={searchText} onChange={e => setSearchText(e.target.value)} />
             <Button> <span className="fa fa-search" /></Button>
         </form>
     );
