936113dbe16b9471578c7babb4553f5e34fad15b
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2022-10-26 17:06:41 +0200

    Install Redux. Add store and initial stor state. Refactor CardContainer and Column components.

--- exercise1/src/components/Column/Column.js
+++ exercise1/src/components/Column/Column.js
@@ -1,9 +1,13 @@
+import { useSelector } from 'react-redux';
+
 import Card from '../Card/Card.js';
 import CardForm from '../CardForm/CardForm.js';
 
 import styles from './Column.module.scss';
 
-const Column = ({ id, title, icon, cards, action }) => {
+const Column = ({ id, title, icon, action }) => {
+
+    const cards = useSelector(state => state.cards.filter(card => card.columnId === id));
 
     return (
         <article className={styles.column}>
--- exercise1/src/index.js
+++ exercise1/src/index.js
@@ -1,4 +1,7 @@
 import ReactDOM from 'react-dom';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
 import App from './App';
 
 
@@ -8,6 +11,10 @@ import './styles/global.scss';
 
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.querySelector('#root')
 );
