1e11d85092d6b7f00b9a876a1b1893fd94b52659
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2022-11-02 20:39:26 +0100

    Create NavBar

--- exercise1/src/App.js
+++ exercise1/src/App.js
@@ -2,11 +2,14 @@ import Hero from "./components/Hero/Hero";
 import SearchForm from "./components/SearchForm/SearchForm";
 import Container from "./components/Container/Container";
 import MainContainer from "./components/MainContainer/MainContainer";
+import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
   return (
     <div>
       <MainContainer>
+        <NavBar />
+        {/* dodac kontener na Main site i inne podstrony - Zrobic jakis jeden komponent i przekazywac childern dla niego w przypadku pod stron*/}
         <Hero />
         <SearchForm />
         <Container />
